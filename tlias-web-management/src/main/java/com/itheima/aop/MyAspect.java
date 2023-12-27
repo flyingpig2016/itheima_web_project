ff93d6a1d6504714b4bee09218c75e338da9cc6b
flyingpig <10423935+flyingpig2022@user.noreply.gitee.com>
2023-12-28 00:58:23 +0800

    commit aop core

---
+++ tlias-web-management/src/main/java/com/itheima/aop/MyAspect.java
@@ -0,0 +1,48 @@
+package com.itheima.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class MyAspect {
+
+    //    声明成public，其他地方可以使用pt()
+    @Pointcut("execution(* com.itheima.service.impl.DeptServiceImpl.*(..))")
+    public void pt() {
+    }
+
+    @Before("pt()")
+    public void before() {
+        log.info("before...");
+    }
+
+    @Around("pt()")
+    public Object around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        log.info("around before...");
+//        调用目标对象的原始方法
+        Object result = proceedingJoinPoint.proceed();
+
+        log.info("around after...");
+
+        return result;
+    }
+
+    @After("pt()")
+    public void after() {
+        log.info("after...");
+    }
+
+    @AfterReturning("pt()")
+    public void afterReturning() {
+        log.info("afterReturning...");
+    }
+
+    @AfterThrowing("pt()")
+    public void afterThrowing() {
+        log.info("afterThrowing...");
+    }
+}
