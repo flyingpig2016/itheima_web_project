6e6b78762a82598a195cf07e75e8fcf06388b5f2
flyingpig <10423935+flyingpig2022@user.noreply.gitee.com>
2023-12-27 23:00:13 +0800

    AOP start

---
+++ tlias-web-management/src/main/java/com/itheima/aop/TimeAspect.java
@@ -0,0 +1,31 @@
+package com.itheima.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Aspect //AOP类
+public class TimeAspect {
+
+    @Around("execution(* com.itheima.service.*.*(..))") //切入点表达式
+    public Object rocordTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        // 1. 记录开始时间
+        long start = System.currentTimeMillis();
+
+        // 2. 调用原始方法运行
+        Object result = joinPoint.proceed();
+
+        // 3. 记录结束时间
+        long end = System.currentTimeMillis();
+        // 4.  计算运行时间
+
+        long duration = end - start;
+        log.info(joinPoint.getSignature() + "方法执行耗时: {}ms", duration);
+
+        return result;
+    }
+}
