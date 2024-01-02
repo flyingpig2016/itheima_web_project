79ace02ba88624fb334af6b2c43995494ca5f008
flyingpig <flyingpig2016.com>
2024-01-03 00:05:18 +0800

    commit bean manage

--- springboot-web-config2/src/main/java/com/itheima/config/CommonConfig.java
+++ springboot-web-config2/src/main/java/com/itheima/config/CommonConfig.java
@@ -11,6 +11,7 @@ public class CommonConfig {
     //声明第三方bean
     @Bean //将当前方法的返回值对象交给IOC容器管理, 成为IOC容器bean
           //通过@Bean注解的name/value属性指定bean名称, 如果未指定, 默认是方法名
+          //第三方bean如果需要依赖注入，就直接在参数里面注入，如下所示
     public SAXReader reader(DeptService deptService){
         System.out.println(deptService);
         return new SAXReader();
