63ff111b7fe5bc261dac25bfd50e4cfad006f36d
薛棒棒 <xuebb3@asiainfo.com>
2025-11-18 11:47:03 +0800

    提交前端代码

---
+++ vue-project/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    root: true,
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    env: {
+        browser: true,
+        node: true,
+        es6: true
+    },
+    extends: [
+        'plugin:vue/essential',
+        'eslint:recommended'
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    }
+}
+
